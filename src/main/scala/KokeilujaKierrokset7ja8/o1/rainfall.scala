001d81300a3b0eed4542c713d017a08b9d4a2bd4
Klaus Helenius <klaus.helenius@gmail.com>
2017-11-08 00:00:30 +0200

    rainfall

--- src/main/scala/KokeilujaKierrokset7ja8/o1/rainfall.scala
+++ src/main/scala/KokeilujaKierrokset7ja8/o1/rainfall.scala
@@ -7,7 +7,7 @@ import collection.immutable.Vector
 
 object rainfall {
   def averageRainfall(v: Vector[Int]): Option[Int] = {
-    val w: Vector[Int] = v.takeWhile( _ < 999999 ).filter( _ > 0 )
+    val w: Vector[Int] = v.takeWhile( _ < 999999 ).filter( _ >= 0 )
     if (w.size > 0) Some(w.sum / w.size) else None
   }
 }
