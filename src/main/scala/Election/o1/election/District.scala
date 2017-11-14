df891720c884ecb2560e5eacdbe6a37fbe660991
Klaus Helenius <klaus.helenius@gmail.com>
2017-11-14 19:56:16 +0200

    9

--- src/main/scala/Election/o1/election/District.scala
+++ src/main/scala/Election/o1/election/District.scala
@@ -3,37 +3,34 @@ package o1.election
 
 class District(val name: String, val seats: Int, val candidates: Vector[Candidate]) {
 
-  //def candidatesByParty: Map[String, Vector[Candidate]]
-  //NOTE TO STUDENTS: THIS METHOD IS ONLY INTENDED FOR IMPLEMENTATION IN CHAPTER 9.4!
-
+  def candidatesByParty: Map[String, Vector[Candidate]] = this.candidates.groupBy(_.party)
   def candidatesFrom(party: String): Vector[Candidate] = this.candidates.filter(_.party == party)
 
-  //def distributionFigures: Map[Candidate, Double]
-  //NOTE TO STUDENTS: THIS METHOD IS ONLY INTENDED FOR IMPLEMENTATION IN CHAPTER 9.4!
-  //def electedCandidates: Vector[Candidate]
-  //NOTE TO STUDENTS: THIS METHOD IS ONLY INTENDED FOR IMPLEMENTATION IN CHAPTER 9.4!
+  def distributionFigures: Map[Candidate, Double] = {
+    def distributionFigure(p: (Candidate, Int)) = votesByParty(p._1.party) / ( p._2 + 1.0 )
+    rankingsWithinParties.flatMap(_._2.zipWithIndex).map(p => p._1 -> distributionFigure(p))
+  }
+
+  def electedCandidates: Vector[Candidate] = distributionFigures.toVector.sortBy(_._2).reverse.map(_._1).take(this.seats)
 
   def printCandidates(): Unit = {
     for (c <- candidates)
       println(c)
   }
 
-  //def rankingOfParties: Vector[String]
-  //NOTE TO STUDENTS: THIS METHOD IS ONLY INTENDED FOR IMPLEMENTATION IN CHAPTER 9.4!
-  //def rankingsWithinParties: Map[String, Vector[Candidate]]
-  //NOTE TO STUDENTS: THIS METHOD IS ONLY INTENDED FOR IMPLEMENTATION IN CHAPTER 9.4!
+  def rankingOfParties: Vector[String] = votesByParty.toVector.sortBy(_._2).reverse.map(_._1)
+  def rankingsWithinParties: Map[String, Vector[Candidate]] = candidatesByParty.mapValues(_.sortBy(_.votes).reverse)
 
   override
   def toString(): String = "%s: %d candidates, %d seats".format(this.name, this.candidates.size, this.seats)
 
   def topCandidate: Candidate = this.candidates.maxBy(_.votes)
 
-  //def topCandidatesByParty: Map[String, Candidate]
-  //NOTE TO STUDENTS: THIS METHOD IS ONLY INTENDED FOR IMPLEMENTATION IN CHAPTER 9.4!
+  def topCandidatesByParty: Map[String, Candidate] = candidatesByParty.mapValues(_.maxBy(_.votes))
 
   def totalVotes(party: String): Int = this.candidatesFrom(party).foldLeft(0)(_ + _.votes)
   def totalVotes: Int = this.candidates.foldLeft(0)(_ + _.votes)
 
-  //def votesByParty: Map[String, Int]
-  //NOTE TO STUDENTS: THIS METHOD IS ONLY INTENDED FOR IMPLEMENTATION IN CHAPTER 9.4!
+  def votesByParty: Map[String, Int] = candidatesByParty.mapValues(_.map(_.votes).sum)
+
 }
--- src/main/scala/KokeilujaKierros9/o1/io/Tilastoja.scala
+++ src/main/scala/KokeilujaKierros9/o1/io/Tilastoja.scala
@@ -6,27 +6,31 @@ import scala.io.StdIn._
 // TÄMÄ TEHTÄVÄ ON ESITELTY KURSSIMATERIAALIN LUVUSSA 9.3.
 
 object Tilastoja extends App {
-  
-  val tiedostonNimi = readLine("Syötetiedosto: ")
-  
-  // TÄYDENNÄ TIEDOSTONKÄSITTELYKOODISI TÄHÄN ALLE
-  
-  // TODO
-  
-  
-  // VOIT KÄYTTÄÄ NÄITÄ VALMIITA FUNKTIOITA:
-  
+
   def keskiarvo(lukuja: Vector[Double]) = lukuja.sum / lukuja.size
-  
+
   def mediaani(lukuja: Vector[Double]) = {
     val puolivali = lukuja.size / 2
     val jarjestetyt = lukuja.sorted
-    if (lukuja.size % 2 == 1) 
+    if (lukuja.size % 2 == 1)
       jarjestetyt(puolivali)
     else
       (jarjestetyt(puolivali - 1) + jarjestetyt(puolivali)) / 2
   }
-  
+
   def yleisimmanMaara(lukuja: Vector[Double]) = lukuja.groupBy(identity).values.map( _.size ).max
+
+  val fname = readLine("Syötetiedosto: ")
+  val ifile = Source.fromFile(fname)
+
+  try {
+    val values = ifile.getLines.toVector.map(_.toDouble)
+    println("Lukumäärä: " + values.size)
+    println("Keskiarvo: " + keskiarvo(values))
+    println("Mediaani: " + mediaani(values))
+    println("Yleisimmän määrä:" + yleisimmanMaara(values))
+  } finally {
+    ifile.close()
+  }
 
 }
