9e76932a2574ac64d854b9f5ac62b7c35abd75f1
Klaus Helenius <klaus.helenius@gmail.com>
2017-11-30 02:20:05 +0200

    family

--- src/main/scala/KokeilujaKierros11/o1/family/Person.scala
+++ src/main/scala/KokeilujaKierros11/o1/family/Person.scala
@@ -8,35 +8,35 @@ package o1.family
   *
   * A person object is immutable after creation.
   *
-  * @param name      the person's name 
+  * @param name      the person's name
   * @param children  the person's children */
 class Person(val name: String, val children: Vector[Person]) {
-  
+
   /** Initializes a new, childless `Person`. */
   def this(name: String) = this(name, Vector())
-  
-  
+
+
   /** Returns the number of children the person has. */
   def numberOfChildren = this.children.size
 
-  
-  /** Returns the number of descendants the person has in total. A person's 
+
+  /** Returns the number of descendants the person has in total. A person's
     * descendants are that person's children plus all their descendants. */
   def numberOfDescendants: Int = ??? // TODO: replace with a working implementation
-  
-  
+
+
   /** Returns the number of descendants the person has in a particular generation.
-    * A generation, is defined here as a number of "steps" indicating how far removed a set 
-    * of descendants is from their ancestor. For example, a person's children form a generation 
-    * at step 1, the grandchildren at step 2, the greatgrandchildren at step 3, and so on. 
+    * A generation, is defined here as a number of "steps" indicating how far removed a set
+    * of descendants is from their ancestor. For example, a person's children form a generation
+    * at step 1, the grandchildren at step 2, the greatgrandchildren at step 3, and so on.
     * @param generation  a positive number of "steps" that identifies a generation
-    * @return the number of descendants that are *exactly* the indicated number of steps 
+    * @return the number of descendants that are *exactly* the indicated number of steps
     *         removed from the person in the family tree */
   def numberOfDescendantsAt(generation: Int): Int = ??? // TODO: replace with a working implementation
-  
-  
+
+
   /** Returns a string representation of the person. */
-  override def toString = (this.name + "(" + this.children.mkString(",") + ")").take(500) 
-  
-  
+  override def toString = (this.name + "(" + this.children.mkString(",") + ")").take(500)
+
+
 }
--- src/main/scala/RandomText/o1/randomtext/gui/RandomTextApp.scala
+++ src/main/scala/RandomText/o1/randomtext/gui/RandomTextApp.scala
@@ -5,21 +5,23 @@ import scala.swing.event._
 import o1.randomtext.RandomTextGenerator
 
 /** The singleton object `RandomTextApp` represents the Random Text application.
-  * The object serves as a possible entry point for the game, and can be run to  
-  * start up a graphical user interface. 
+  * The object serves as a possible entry point for the game, and can be run to
+  * start up a graphical user interface.
   *
-  * The application provides end user access to the functionality provided by the 
-  * class [[o1.randomtext.RandomTextGenerator]]. There is a text field for providing 
-  * a data source for random text generation, and a text area for displaying the 
-  * text produced. A button is available that the user can click to indicate that 
+  * The application provides end user access to the functionality provided by the
+  * class [[o1.randomtext.RandomTextGenerator]]. There is a text field for providing
+  * a data source for random text generation, and a text area for displaying the
+  * text produced. A button is available that the user can click to indicate that
   * they wish to generate a new batch of nonsense.
   *
   * '''NOTE TO STUDENTS: This class is discussed in further detail in Chapter 11.2
   * of the course materials.''' */
 object RandomTextApp extends SimpleSwingApplication {
 
-  // Components: 
-  
+  val randomTextGenerator = new RandomTextGenerator(9)
+
+  // Components:
+
   val prompt = new Label("Source file or URL:")
   val sourceField = new TextField("alice.txt", 50)
   val randomizeButton = new Button("Randomize!")
@@ -27,9 +29,10 @@ object RandomTextApp extends SimpleSwingApplication {
   val outputArea = new TextArea("Random stuff will appear here.", 30, 85)
   outputArea.editable = false
   outputArea.lineWrap = true
+  outputArea.wordWrap = true
 
-  // Layout: 
-  
+  // Layout:
+
   val topRow = new FlowPanel
   topRow.contents += prompt
   topRow.contents += sourceField
@@ -38,14 +41,19 @@ object RandomTextApp extends SimpleSwingApplication {
   val wholeLayout = new BoxPanel(Orientation.Vertical)
   wholeLayout.contents += topRow
   wholeLayout.contents += outputArea
-  
+
   val window = new MainFrame
   window.title = "Random Text Generator"
   window.resizable = false
   window.contents = wholeLayout
-  
+
   def top = this.window
-  
+
+  listenTo(randomizeButton)
+  reactions += {
+    case ButtonClicked(_) =>
+      outputArea.text = randomTextGenerator.randomize(sourceField.text)
+  }
 }
 
 
